e5f69c23ca6842e85136e63a023d969c38b9814d
anahitabozorgi <bzanahita79@gmail.com>
2021-12-07 11:48:13 +0330

    success or fail register

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -10,5 +10,24 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
+    function create(Request $request){
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required|email|unique:users,email',
+            'password'=>'required|min:8|max:30',
+            'cpassword'=>'required|min:8|max:30|same:password'
+        ]);
 
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = \Hash::make($request->password);
+        $save = $user->save();
+
+        if($save){
+            return redirect()->back()->with('success', 'you are registered');
+        }else{
+            return redirect()->back()->with('fail', 'something went wrong');
+        }
+    }
 }
