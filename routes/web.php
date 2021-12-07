ddb32c645cd53514aefdf3257b822a2c4a51dd52
anahitabozorgi <bzanahita79@gmail.com>
2021-12-07 12:42:24 +0330

    make PreventBackHistory

--- routes/web.php
+++ routes/web.php
@@ -23,14 +23,14 @@ Auth::routes();
 
 Route::prefix('user')->name('user.')->group(function(){
   
-    Route::middleware(['guest'])->group(function(){
+    Route::middleware(['guest','PreventBackHistory'])->group(function(){
           Route::view('/login','dashboard.user.login')->name('login');
           Route::view('/register','dashboard.user.register')->name('register');
           Route::post('/create',[UserController::class,'create'])->name('create');
           Route::post('/check',[UserController::class,'check'])->name('check');
     });
 
-    Route::middleware(['auth'])->group(function(){
+    Route::middleware(['auth','PreventBackHistory'])->group(function(){
           Route::view('/home','dashboard.user.home')->name('home');
           Route::post('/logout',[UserController::class,'logout'])->name('logout');
 
