86c985b14d26810c16fb460529c54d6e377cbc70
anahitabozorgi <bzanahita79@gmail.com>
2021-12-27 23:12:15 +0330

    make "create" function in controller

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -24,6 +24,27 @@ class AdminController extends Controller
             return redirect()->route('admin.login')->with('fail','incorrect data');
         }
     }
+
+    function create(Request $request){
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required|email|unique:admins,email',
+            'password'=>'required|min:8|max:30',
+            'cpassword'=>'required|min:8|max:30|same:password'
+        ]);
+
+        $admin = new Admin();
+        $admin->name = $request->name;
+        $admin->email = $request->email;
+        $admin->password = \Hash::make($request->password);
+        $save = $admin->save();
+
+        if($save){
+            return redirect()->back()->with('success', 'New admin has added');
+        }else{
+            return redirect()->back()->with('fail', 'something went wrong');
+        }
+    }
     
     function logout(){
         Auth::guard('admin')->logout();
