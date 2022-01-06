858eb4940c7607323b6233b18d27cf4a3e89068a
anahitabozorgi <bzanahita79@gmail.com>
2022-01-07 03:24:06 +0330

    make an 'add to cart button' for each user

---
+++ app/Models/Cart.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title2','image2','price2','email1'];
+}
