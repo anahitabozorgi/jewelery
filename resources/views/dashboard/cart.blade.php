255155f40c2958adb15d807352d5f54795250e73
anahitabozorgi <bzanahita79@gmail.com>
2022-01-07 03:37:57 +0330

    make an 'add to cart button' for each user

--- resources/views/dashboard/cart.blade.php
+++ resources/views/dashboard/cart.blade.php
@@ -11,11 +11,11 @@
     <td><img src="{{asset('images')}}/{{$product->image2}}" style="max-width:200px;"/></td>
     <input type="hidden" {{$sum = $sum+(int)$product->price2}}>
 <br>
+    <form action="{{route('user.cart.destroy' , ['id' => $product->id])}}" method="post">
+        @csrf
+        <button type="submit">delete</button>
+    </form>
     <br>
 </tr>
 @endforeach
 <h3>Total:{{$sum}}$</h3>
-<form action="{{route('user.cart.destroy' , ['id' => $product->id])}}" method="post">
-    @csrf
-    <button type="submit">delete</button>
-</form>
