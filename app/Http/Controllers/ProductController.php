430bf94f24d7677555af65d0f7a486ad4f2188ca
anahitabozorgi <bzanahita79@gmail.com>
2022-01-09 01:38:18 +0330

    make an 'add to cart button' for each user

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -243,15 +243,11 @@ class ProductController extends Controller
     }
 
     public function search(Request $request){
-        // Get the search value from the request
         $search = $request->input('search');
-    
-        // Search in the title and body columns from the posts table
+
         $products = Product::query()
             ->where('title1', 'LIKE', "%{$search}%")
             ->get();
-    
-        // Return the search view with the resluts compacted
         return view('dashboard.search', compact('products'));
     }
 }
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -16,12 +16,33 @@
             <a href="{{route('bracelet')}}">Bracelet</a>
             <a href="{{route('earing')}}">Earing</a>
             <a href="{{route('necklaces')}}">Necklaces</a>
+            <form method="get" action="{{route('user.product.cartshow')}}" enctype="multipart/form-data">
+                @csrf
+                <input type="hidden" name="id" value="{{Auth::guard('web')->user()->id}}">
+                <input class="button" type="submit" value="your shopping cart"> 
+            </form>
+            @if (Session::get('success'))
+            <div class="alert alert-success">
+                {{Session::get('success')}}
+            </div>   
+            @endif
+        
+            @if(Session::get('fail'))
+            <div class="alert alert-danger">
+                {{Session::get('fail')}}
+            </div>
+            @endif
             @foreach ($products as $product)
             <tr>
                 <td>{{$product->title1}}</td><br>
                 <td>{{$product->price1}}$</td>
                 <td><img src="{{asset('images')}}/{{$product->image1}}" style="max-width:200px;"/></td>
                 <td><a href="{{route('user.product.show',['id' => $product->id])}}">details</a></td>
+                <td><form method="post" action="{{route('user.product.cart',['id' => $product->id])}}" enctype="multipart/form-data">
+                    @csrf
+                    <input type="hidden" name="id" value="{{Auth::guard('web')->user()->id}}">
+                    <input type="submit" value="Add to Cart"> 
+                </form></td>
             </tr>
         @endforeach
         @else
